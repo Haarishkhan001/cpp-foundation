efa54d5a797347c11be5dfe46677118ea7650cad
Haarish Khan <haarishkhan032@gmail.com>
2026-06-18 16:59:05 +0000

    Added basics.cpp and maths_ope.cpp

---
+++ 01_C++_Basics/mosh_tutorial/basics.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main (){
+    int id_name=100, counter=74;
+    double fridge_size=7995.4;
+    std::cout <<counter ;
+    return 0;
+}
---
+++ 01_C++_Basics/mosh_tutorial/maths_ope.cpp
@@ -0,0 +1,36 @@
+// #include <iostream>
+// int main (){
+//     double a=10;
+//     int b=3;
+//     double c=a/b;
+//     std::cout << c;
+// }
+
+
+// #include <iostream>
+// int main (){
+// int a=10;          this is a increment func
+// int b=a++;
+// std::cout << a;  answer here is 11
+// return 0;
+// }
+
+
+// #include <iostream>
+// int main (){
+// int a=10;
+// int b=a++;
+// std::cout << b;     answer here is 10
+// return 0;
+// }
+
+
+#include <iostream>
+int main (){
+int a=10;
+int b=++a;     //b=11, c=11
+//int c=a++;  //c=10, a=11
+std::cout << b;       
+return 0;
+}
+
