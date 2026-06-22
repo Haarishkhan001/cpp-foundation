a07ae5609f1c2254f58dc0987db31340c6dacf25
Haarish Khan <haarishkhan032@gmail.com>
2026-06-22 18:53:11 +0000

    Add vari-func.cpp tutorial file

---
+++ 01_C++_Basics/cherno_tutorial/vari-func.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main (){
+    // char="A";  1 bit 
+    // short=243; 2 bits
+    // int =432; 4bits
+    // long=3431; 4bits
+    // long long=34; 8bits
+
+
+    // float=21.34;
+    double left=314.542;git add vari-func.cpp
+git commit -m "Add vari-func.cpp tutorial file"
+git push
+    cout<< left << endl;
+}
