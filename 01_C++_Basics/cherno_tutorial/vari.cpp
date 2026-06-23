08bd3921b0395371d61743d3a40e4ef406b17ed9
Haarish Khan <haarishkhan032@gmail.com>
2026-06-23 19:58:15 +0000

    Add changes to vari.cpp

---
+++ 01_C++_Basics/cherno_tutorial/vari.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main (){
+    // char="A";  1 bit 
+    // short=243; 2 bits
+    // int =432; 4bits
+    // long=3431; 4bits
+    // long long=34; 8bits
+
+
+    // float=21.34;
+    double left=314.542;
+    cout << left << endl;
+}
